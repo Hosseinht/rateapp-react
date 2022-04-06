63cc0dd061a6f8e34dd98e148260d271e9934d2c
Ryan <sailantht@gmail.com>
2022-04-06 14:59:59 +0430

    Fade Animation

--- src/components/RateList.jsx
+++ src/components/RateList.jsx
@@ -1,22 +1,45 @@
+import {motion, AnimatePresence} from "framer-motion";
 import RateItem from "./RateItem";
 
 
-const RateList = ({rateData , reverse,handleDelete}) => {
+const RateList = ({rateData, reverse, handleDelete}) => {
     if (!rateData || rateData.length === 0) {
         return <p>No rate yet</p>
     }
     return (
         <div className='feedback-list'>
-            {rateData.map((item) => (
-                <RateItem
-                    reverse={reverse}
-                    key={item.id}
-                    item={item}
-                    handleDelete={handleDelete}
-                />
-            ))}
+            <AnimatePresence>
+                {rateData.map((item) => (
+                    <motion.div
+                        key={item.id}
+                        initial={{opacity: 0}}
+                        animate={{opacity: 1}}
+                        exit={{opacity: 0}}
+                    >
+                        <RateItem
+                            reverse={reverse}
+                            key={item.id}
+                            item={item}
+                            handleDelete={handleDelete}
+                        />
+                    </motion.div>
+                ))}
+            </AnimatePresence>
         </div>
     );
+    // return (
+    //     <div className='feedback-list'>
+    //         {rateData.map((item) => (
+    //             <RateItem
+    //                 reverse={reverse}
+    //                 key={item.id}
+    //                 item={item}
+    //                 handleDelete={handleDelete}
+    //             />
+    //         ))}
+    //     </div>
+    // );
+    //
 };
 
 export default RateList;
